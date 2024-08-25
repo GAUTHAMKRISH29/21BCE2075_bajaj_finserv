3c4124a12078578fb61e3a9db63c188a978893d9
GAUTHAMKRISH29 <gauthamposani11@gmail.com>
2024-08-25 12:49:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ def handle_bfhl():
         highest_lowercase = max(lowercase_alphabets) if lowercase_alphabets else None
 
         response = {
-           "is_success": True,
-            "user_id": "john_doe_17091999",
-            "email": "john@xyz.com",
-            "roll_number": "ABCD123",
+            "is_success": True,
+            "user_id": "gautham29",
+            "email": "gauthamposani11@gmail.com",
+            "roll_number": "21BCE2075",
             "numbers": numbers,
             "alphabets": alphabets,
             "highest_lowercase_alphabet": [highest_lowercase] if highest_lowercase else []
